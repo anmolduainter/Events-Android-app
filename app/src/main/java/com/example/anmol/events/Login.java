103c06704fda2c4d9965fd117648d0d06ac39ced
anmolduainter <anmolduainter@gmail.com>
2017-07-30 14:50:21 +0530

    Login Style

---
+++ app/src/main/java/com/example/anmol/events/Login.java
@@ -0,0 +1,22 @@
+package com.example.anmol.events;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by anmol on 30/7/17.
+ */
+
+public class Login extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_main);
+
+
+
+
+    }
+}
