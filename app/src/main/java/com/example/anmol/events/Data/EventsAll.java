bd7b17699f9453454873924df6ad531477c652da
anmolduainter <anmolduainter@gmail.com>
2017-07-30 14:21:13 +0530

    EventsAll

---
+++ app/src/main/java/com/example/anmol/events/Data/EventsAll.java
@@ -0,0 +1,160 @@
+package com.example.anmol.events.Data;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.anmol.events.Adapter.RecyclerAllEvents;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.*;
+
+/**
+ * Created by anmol on 30/7/17.
+ */
+
+public class EventsAll {
+
+
+    RequestQueue rq;
+    Context ctx;
+    List<String> imgUrl,name,date,time,desc,phone,username;
+    String event;
+    HashMap<String,String> hash=new HashMap<String,String>();
+
+    List<List<String>> Actualli;
+
+    public EventsAll(Context ctx, String event){
+        this.event=event;
+        this.ctx=ctx;
+        hash.put("AllEvents","http://192.168.0.105:3000/Events/JSON");
+
+    }
+
+
+    public void getAll(final VolleyCallback callback){
+
+        String URL = null;
+
+        for(Map.Entry m:hash.entrySet()){
+
+            if (m.getKey().equals(event)){
+                URL= (String) m.getValue();
+            }
+
+        }
+
+        rq= Volley.newRequestQueue(ctx);
+
+        // final ProgressDialog progressDialog = ProgressDialog.show(getApplicationContext(), "", "Pro Start", false, false);
+        JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.GET,URL, null,
+
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+
+                        imgUrl=new ArrayList<>();
+                        name=new ArrayList<>();
+                        date=new ArrayList<>();
+                        time=new ArrayList<>();
+                        desc=new ArrayList<>();
+                        phone=new ArrayList<>();
+                        username=new ArrayList<>();
+                        Actualli=new ArrayList<>();
+
+                        try {
+                            //                   progressDialog.cancel();
+                            JSONArray jsonArray=response.getJSONArray("Result");
+
+                            for (int i=0;i<jsonArray.length();i++){
+
+                                JSONObject object=jsonArray.getJSONObject(i);
+
+                                //  System.out.println(object);
+
+                                imgUrl.add(object.getString("imgUrl"));
+                                name.add(object.getString("name"));
+                                date.add(object.getString("date"));
+                                time.add(object.getString("time"));
+                                desc.add(object.getString("desc"));
+//                                phone.add(object.getString("phone"));
+//                                username.add(object.getString("username"));
+
+                                System.out.println(imgUrl.get(i));
+                                System.out.println(name.get(i));
+                                System.out.println(date.get(i));
+                                System.out.println(time.get(i));
+
+
+
+                            }
+
+                            JSONArray jsonArray1=response.getJSONArray("Arr");
+                            for (int i=0;i<jsonArray1.length();i++){
+
+                                JSONObject object=jsonArray1.getJSONObject(i);
+                                phone.add(object.getString("phone"));
+                                username.add(object.getString("username"));
+
+                            }
+
+                            Actualli.add(imgUrl);
+                            Actualli.add(name);
+                            Actualli.add(date);
+                            Actualli.add(time);
+                            Actualli.add(desc);
+
+
+                            Actualli.add(phone);
+                            Actualli.add(username);
+
+
+
+
+                            callback.onSuccess(Actualli);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        Log.i("Message :  ",name.get(0));
+
+                    }
+
+
+
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+
+
+                    }
+                }
+
+        );
+
+        rq.add(jsonObjectRequest);
+
+    }
+
+
+    public interface VolleyCallback{
+        void onSuccess(List<List<String>> result);
+    }
+
+}
--- app/src/main/java/com/example/anmol/events/Events/AllEvents.java
+++ app/src/main/java/com/example/anmol/events/Events/AllEvents.java
@@ -1,6 +1,7 @@
 package com.example.anmol.events.Events;
 
 import android.app.ProgressDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
@@ -18,6 +19,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.anmol.events.Adapter.RecyclerAllEvents;
+import com.example.anmol.events.Data.EventsAll;
 import com.example.anmol.events.R;
 
 import org.json.JSONArray;
@@ -33,7 +35,7 @@ import java.util.List;
 
 public class AllEvents extends AppCompatActivity {
 
-    String URL="http://192.168.0.105:3000/Events";
+    String URL="http://192.168.0.105:3000/Events/JSON";
     RequestQueue rq;
     Toolbar toolbar;
     CollapsingToolbarLayout collapsingToolbarLayout;
@@ -54,91 +56,138 @@ public class AllEvents extends AppCompatActivity {
 
         rv= (RecyclerView) findViewById(R.id.recyclerAllEvents);
 
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
 
 
-        rq=Volley.newRequestQueue(AllEvents.this);
+            int scroll=-1;
 
-       // final ProgressDialog progressDialog = ProgressDialog.show(getApplicationContext(), "", "Pro Start", false, false);
-        JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.GET,URL, null,
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
 
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
+                if (scroll==-1){
 
 
-                        imgUrl=new ArrayList<>();
-                        name=new ArrayList<>();
-                        date=new ArrayList<>();
-                        time=new ArrayList<>();
-                        desc=new ArrayList<>();
-                        phone=new ArrayList<>();
-                        username=new ArrayList<>();
+                    scroll=appBarLayout.getTotalScrollRange();
 
-                        try {
-         //                   progressDialog.cancel();
-                            JSONArray jsonArray=response.getJSONArray("Result");
+                }
 
-                            for (int i=0;i<jsonArray.length();i++){
+                if (verticalOffset * -1 >= scroll-150 ){
+                    collapsingToolbarLayout.setTitle("EventsAll");
+                    collapsingToolbarLayout.setCollapsedTitleTextColor(Color.WHITE);
 
-                                JSONObject object=jsonArray.getJSONObject(i);
+                }
 
-                              //  System.out.println(object);
+                else{
 
-                                imgUrl.add(object.getString("imgUrl"));
-                                name.add(object.getString("name"));
-                                date.add(object.getString("date"));
-                                time.add(object.getString("time"));
-                                desc.add(object.getString("desc"));
-//                                phone.add(object.getString("phone"));
-//                                username.add(object.getString("username"));
+                    collapsingToolbarLayout.setTitle(" ");
 
-                                System.out.println(imgUrl.get(i));
-                                System.out.println(name.get(i));
-                                System.out.println(date.get(i));
-                                System.out.println(time.get(i));
-
-                            }
-
-                            JSONArray jsonArray1=response.getJSONArray("Arr");
-                            for (int i=0;i<jsonArray1.length();i++){
-
-                                JSONObject object=jsonArray1.getJSONObject(i);
-                                phone.add(object.getString("phone"));
-                                username.add(object.getString("username"));
-
-                            }
+                }
 
 
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+            }
+        });
 
-                        Log.i("Message :  ",name.get(0));
+
+        EventsAll allData=new EventsAll(AllEvents.this,"AllEvents");
+
+        allData.getAll(new EventsAll.VolleyCallback() {
+            @Override
+            public void onSuccess(List<List<String>> result) {
 
 
                         layoutManager=new LinearLayoutManager(AllEvents.this);
                         rv.setLayoutManager(layoutManager);
-                        adapter=new RecyclerAllEvents(AllEvents.this,imgUrl,name,date,time,desc,phone,username);
+                        adapter=new RecyclerAllEvents(AllEvents.this,result.get(0),result.get(1),result.get(2),result.get(3),result.get(4),result.get(5),result.get(6));
                         rv.setAdapter(adapter);
 
-                    }
+
+            }
+        });
 
 
-
-                },
-
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-
-
-                    }
-                }
-
-        );
-
-        rq.add(jsonObjectRequest);
+//        rq=Volley.newRequestQueue(EventsAll.this);
+//
+//       // final ProgressDialog progressDialog = ProgressDialog.show(getApplicationContext(), "", "Pro Start", false, false);
+//        JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.GET,URL, null,
+//
+//                new Response.Listener<JSONObject>() {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//
+//
+//                        imgUrl=new ArrayList<>();
+//                        name=new ArrayList<>();
+//                        date=new ArrayList<>();
+//                        time=new ArrayList<>();
+//                        desc=new ArrayList<>();
+//                        phone=new ArrayList<>();
+//                        username=new ArrayList<>();
+//
+//                        try {
+//         //                   progressDialog.cancel();
+//                            JSONArray jsonArray=response.getJSONArray("Result");
+//
+//                            for (int i=0;i<jsonArray.length();i++){
+//
+//                                JSONObject object=jsonArray.getJSONObject(i);
+//
+//                              //  System.out.println(object);
+//
+//                                imgUrl.add(object.getString("imgUrl"));
+//                                name.add(object.getString("name"));
+//                                date.add(object.getString("date"));
+//                                time.add(object.getString("time"));
+//                                desc.add(object.getString("desc"));
+////                                phone.add(object.getString("phone"));
+////                                username.add(object.getString("username"));
+//
+//                                System.out.println(imgUrl.get(i));
+//                                System.out.println(name.get(i));
+//                                System.out.println(date.get(i));
+//                                System.out.println(time.get(i));
+//
+//                            }
+//
+//                            JSONArray jsonArray1=response.getJSONArray("Arr");
+//                            for (int i=0;i<jsonArray1.length();i++){
+//
+//                                JSONObject object=jsonArray1.getJSONObject(i);
+//                                phone.add(object.getString("phone"));
+//                                username.add(object.getString("username"));
+//
+//                            }
+//
+//
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                        }
+//
+//                        Log.i("Message :  ",name.get(0));
+//
+//
+//                        layoutManager=new LinearLayoutManager(EventsAll.this);
+//                        rv.setLayoutManager(layoutManager);
+//                        adapter=new RecyclerAllEvents(EventsAll.this,imgUrl,name,date,time,desc,phone,username);
+//                        rv.setAdapter(adapter);
+//
+//                    }
+//
+//
+//
+//                },
+//
+//                new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//
+//
+//
+//                    }
+//                }
+//
+//        );
+//
+//        rq.add(jsonObjectRequest);
 
     }
 }
