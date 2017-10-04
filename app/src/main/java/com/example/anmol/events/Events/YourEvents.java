6286124ae3f1e262a84b96ab50f2f1549acf0c17
anmolduainter <anmolduainter@gmail.com>
2017-10-05 00:57:19 +0530

    .

--- app/src/main/java/com/example/anmol/events/Events/YourEvents.java
+++ app/src/main/java/com/example/anmol/events/Events/YourEvents.java
@@ -3,16 +3,20 @@ package com.example.anmol.events.Events;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 
 import com.example.anmol.events.R;
 
 public class YourEvents extends AppCompatActivity{
 
+    private RecyclerView rv;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-       // setContentView(R.layout.);
+        setContentView(R.layout.your_events_main);
     }
 }
